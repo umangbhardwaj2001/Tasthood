9898f050f4e663c5deb927b4cd6670f1dddc3a04
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-03-14 11:13:29 +0530

    add useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,32 @@
-import mockData from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurentCard from "./RestaurentCard";
-
+import BodyShimmer from "./BodyShimmer";
 const Body = () => {
-  const [data, setData] = useState(mockData);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("Working");
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999"
+    );
+    const json = await data.json();
+    cleanJson =
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setData(cleanJson);
+  };
+  if (data.length == 0) {
+    return (
+      <div>
+        <div className="search-container">
+          <input type="text" className="search-input" placeholder="Search..." />
+          <button className="search-button">Search</button>
+        </div>
+        <BodyShimmer />
+      </div>
+    );
+  }
   return (
     <div className="body">
       <div className="search-container">
