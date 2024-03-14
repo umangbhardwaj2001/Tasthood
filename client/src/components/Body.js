616f484f6c2faede80c39153723fb2e3fac14757
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-03-14 12:34:39 +0530

    move react files in client folder

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -8,9 +8,7 @@ const Body = () => {
     fetchData();
   }, []);
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999"
-    );
+    const data = await fetch("http://localhost:3000/fetch-data");
     const json = await data.json();
     cleanJson =
       json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.get("/fetch-data", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+        },
+      }
+    );
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
