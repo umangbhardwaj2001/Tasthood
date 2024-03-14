284592b9a68eb62f4624643e4fbb3d8bc3171ec5
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-03-14 11:14:19 +0530

    update path

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -2,23 +2,16 @@ import { CDN_URL } from "../utils/contant";
 
 const RestaurentCard = (props) => {
   const { resData } = props;
-  const {
-    cloudinaryImageId,
-    name,
-    cuisines,
-    avgRatingString,
-    deliveryTime,
-    areaName,
-  } = resData?.info;
+  const { cloudinaryImageId, name, cuisines, avgRatingString, sla, areaName } =
+    resData?.info;
   return (
     <div className="res-card">
       <div className="res-logo">
         <img src={CDN_URL + cloudinaryImageId} />
-
         <h3>{name}</h3>
         <p>{cuisines.join(", ")}</p>
         <h4>{avgRatingString}</h4>
-        <p>{deliveryTime} Minutes</p>
+        <p>{sla.deliveryTime} Minutes</p>
         <h6>{areaName}</h6>
       </div>
     </div>
